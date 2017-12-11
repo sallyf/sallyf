0616354844b6cd2d44013e7658267b971183ad5d
Raphael Vigee <contact@raphaelvigee.com>
2017-12-11 18:06:04 +0000

    Rename variable

--- src/main/java/com/raphaelvigee/sally/Routing/Routing.java
+++ src/main/java/com/raphaelvigee/sally/Routing/Routing.java
@@ -29,11 +29,11 @@ public class Routing extends ContainerAware
                     continue;
                 }
 
-                com.raphaelvigee.sally.Annotation.Route route = method.getAnnotation(com.raphaelvigee.sally.Annotation.Route.class);
+                com.raphaelvigee.sally.Annotation.Route routeAnnotation = method.getAnnotation(com.raphaelvigee.sally.Annotation.Route.class);
 
                 final Class<?>[] parameterTypes = method.getParameterTypes();
 
-                addAction(route.method(), route.path(), (container, session, routeDefinition) -> {
+                addAction(routeAnnotation.method(), routeAnnotation.path(), (container, session, routeDefinition) -> {
                     Object[] parameters = new Object[parameterTypes.length];
                     for (Class<?> parameterType : parameterTypes) {
                         Object p;
