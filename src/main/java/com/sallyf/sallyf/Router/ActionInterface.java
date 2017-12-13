b234b71519355c9bbe44d7af7bf086674b128757
Raphael Vigee <contact@raphaelvigee.com>
2017-12-13 16:23:38 +0000

    Ass FunctionnalInterface annotation

--- src/main/java/com/sallyf/sallyf/Router/ActionInterface.java
+++ src/main/java/com/sallyf/sallyf/Router/ActionInterface.java
@@ -4,6 +4,7 @@ import com.sallyf.sallyf.Container.Container;
 import com.sallyf.sallyf.Exception.UnhandledParameterException;
 import com.sallyf.sallyf.Server.HTTPSession;
 
+@FunctionalInterface
 public interface ActionInterface
 {
     Response apply(Container c, HTTPSession s, Route r) throws UnhandledParameterException;
