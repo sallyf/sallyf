a89c53623001547142cbb33913a76c943235996f
Raphael Vigee <contact@raphaelvigee.com>
2017-12-13 16:23:38 +0000

    Ass FunctionnalInterface annotation

--- src/main/java/com/raphaelvigee/sally/Router/ActionInterface.java
+++ src/main/java/com/raphaelvigee/sally/Router/ActionInterface.java
@@ -4,6 +4,7 @@ import com.raphaelvigee.sally.Container.Container;
 import com.raphaelvigee.sally.Exception.UnhandledParameterException;
 import com.raphaelvigee.sally.Server.HTTPSession;
 
+@FunctionalInterface
 public interface ActionInterface
 {
     Response apply(Container c, HTTPSession s, Route r) throws UnhandledParameterException;
