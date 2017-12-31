fd1505e456b8adfa04f2acf38362da3ddf794e29
Raphael Vigee <contact@raphaelvigee.com>
2017-12-31 02:35:05 +0100

    Implement server testing

---
+++ src/test/java/com/sallyf/sallyf/Server/ServerTest.java
@@ -0,0 +1,58 @@
+package com.sallyf.sallyf.Server;
+
+import com.sallyf.sallyf.BaseFrameworkTest;
+import com.sallyf.sallyf.Kernel;
+import com.sallyf.sallyf.Router.Router;
+import org.eclipse.jetty.http.HttpFields;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Request;
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ServerTest extends BaseFrameworkTest
+{
+    private org.eclipse.jetty.server.Response servletResponse;
+
+    @Override
+    public void setUp() throws Exception
+    {
+        app = Kernel.newInstance();
+
+        FrameworkServer server = app.getContainer().add(FrameworkServer.class);
+        server.setHandler(new FrameworkHandler(app.getContainer())
+        {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest servletRequest, HttpServletResponse r)
+            {
+                super.handle(target, baseRequest, servletRequest, r);
+
+                servletResponse = (org.eclipse.jetty.server.Response) r;
+            }
+        });
+
+        Router router = app.getContainer().get(Router.class);
+
+        router.addController(TestController.class);
+
+        app.start();
+    }
+
+    @Test
+    public void responseTest() throws Exception
+    {
+        HttpURLConnection http = (HttpURLConnection) new URL(getRootURL() + "/test1").openConnection();
+        http.connect();
+        assertThat("Response Code", http.getResponseCode(), is(HttpStatus.OK_200));
+        assertThat("Content", streamToString(http), is("OK"));
+
+        assertThat("Header", servletResponse.getHeader("test1"), is("hello1"));
+    }
+}
