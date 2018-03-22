1b20ffcfd568dfa994fc942bd676b25bd02186d8
Raphael Vigee <contact@raphaelvigee.com>
2018-03-22 14:48:29 +0000

    Fix textarea null displayed

--- src/main/java/com/sallyf/sallyf/Form/Type/AbstractFormType.java
+++ src/main/java/com/sallyf/sallyf/Form/Type/AbstractFormType.java
@@ -4,6 +4,7 @@ import com.sallyf.sallyf.Form.Form;
 import com.sallyf.sallyf.Form.FormTypeInterface;
 import com.sallyf.sallyf.Form.FormView;
 import com.sallyf.sallyf.Form.Options;
+import com.sallyf.sallyf.Utils.DataUtils;
 import org.eclipse.jetty.server.Request;
 
 import java.util.Arrays;
@@ -17,7 +18,7 @@ public abstract class AbstractFormType<O extends Options, ND> implements FormTyp
     {
         O vars = formView.getVars();
         Map<String, String> attributes = vars.getAttributes();
-        attributes.put("name", formView.getForm().getFullName());
+        attributes.put("name", DataUtils.fallback(formView.getForm().getFullName(), ""));
 
         if (vars.isDisabled()) {
             attributes.put("disabled", "disabled");
